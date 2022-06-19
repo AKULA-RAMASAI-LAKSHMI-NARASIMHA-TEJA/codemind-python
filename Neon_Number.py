ebcbce4e35b2058cd931b18b9d07c662a28df0d4
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-06-19 13:18:19 +0530

    n=int(input()) s=0 sq=n*n while sq:     d=sq%10     s+=d     sq//=10 if s==n:     print('Neon Number') else:     print("Not Neon Number")

--- Neon_Number.py
+++ Neon_Number.py
@@ -1,12 +1,11 @@
 n=int(input())
-sq=n**2
-temp=sq
 s=0
+sq=n*n
 while sq:
     d=sq%10
     s+=d
-    sq=sq//10
+    sq//=10
 if s==n:
-    print("Neon Number")
+    print('Neon Number')
 else:
     print("Not Neon Number")
