e91543eb3037935b91acc6a03b6f7bd9c7143db3
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-07-17 00:11:43 +0530

    s=input() c=0 for i in s:     if ord(i)>=65 and ord(i)<=90 or ord(i)>=97 and ord(i)<=122 or ord(i)==32:         continue     c+=1 print(c)

--- special_characters_count.py
+++ special_characters_count.py
@@ -1,12 +1,7 @@
-a=input()
-sc=0
-for i in a:
-    if i.isalpha():
+s=input()
+c=0
+for i in s:
+    if ord(i)>=65 and ord(i)<=90 or ord(i)>=97 and ord(i)<=122 or ord(i)==32:
         continue
-    elif i.isdigit():
-        continue
-    elif i==' ':
-        continue
-    else:
-        sc+=1
-print(sc)
+    c+=1
+print(c)
