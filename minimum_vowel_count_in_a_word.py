584f8252c5ce2986596dbbd74187e2c61e7dca8e
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-07-17 00:24:28 +0530

    s=input() s=s.lower() s=s.split() b=[] c=0 a=['a','e','i','o','u'] for i in s:     c=0     i=list(i)     for j in i:         if j in a:             c+=1     b.append(c) print(min(b))

---
+++ minimum_vowel_count_in_a_word.py
@@ -0,0 +1,14 @@
+s=input()
+s=s.lower()
+s=s.split()
+b=[]
+c=0
+a=['a','e','i','o','u']
+for i in s:
+    c=0
+    i=list(i)
+    for j in i:
+        if j in a:
+            c+=1
+    b.append(c)
+print(min(b))
