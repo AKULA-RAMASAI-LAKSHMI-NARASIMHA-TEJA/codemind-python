779e6e9fb3bcb72a42a764c6407c4abc036ba414
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-07-18 11:55:33 +0530

    m,n=map(int,input().split()) a=list(map(int,input().split())) b=list(map(int,input().split())) c=[] d=[] for i in a:     if i not in c:         c.append(i) for i in b:     if i not in d:         d.append(i) for i in c:     if i not in b:         print(i,end=' ') for i in d:     if i not in a:         print(i,end=' ')

--- Common_elements_II.py
+++ Common_elements_II.py
@@ -2,10 +2,16 @@ m,n=map(int,input().split())
 a=list(map(int,input().split()))
 b=list(map(int,input().split()))
 c=[]
+d=[]
 for i in a:
-    if i not in b:
+    if i not in c:
         c.append(i)
 for i in b:
+    if i not in d:
+        d.append(i)
+for i in c:
+    if i not in b:
+        print(i,end=' ')
+for i in d:
     if i not in a:
-        c.append(i)
-print(*c)
+        print(i,end=' ')
