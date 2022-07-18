2b3be477040daf6f0a39cf75e5e20f8eab04fa7b
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-07-18 23:09:20 +0530

    s=input() s=s.lower() s=s.split() s=list(s) a='aeiou' c=0 for i in s:     if i[0] in a:         if i[len(i)-1] not in a:             c+=1 print(c)

--- count_words.py
+++ count_words.py
@@ -1,9 +1,11 @@
 s=input()
 s=s.lower()
 s=s.split()
+s=list(s)
+a='aeiou'
 c=0
-a=['a','e','i','o','u']
 for i in s:
-    if i[0] in a and i[-1] not in a:
-        c+=1
+    if i[0] in a:
+        if i[len(i)-1] not in a:
+            c+=1
 print(c)
