855709e2ba3b2cc9c12d1b502570615a61803a8e
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-10-14 21:59:31 +0530

    a=0 s1=(input().lower()).split(' ') s2=(input().lower()).split(' ') for i in s1:     for j in s2:         if i==j and s1.count(i)==1 and s2.count(j)==1:             a+=1 print(a)

--- Common_words_-2.py
+++ Common_words_-2.py
@@ -1,10 +1,8 @@
-a=input()
-b=input()
-a,b=a.lower(),b.lower()
-a,b=a.split(),b.split()
-a,b=list(a),list(b)
-c=0
-for i in a:
-    if b.count(i)==1 and a.count(i)==1:
-        c+=1
-print(c)
+a=0
+s1=(input().lower()).split(' ')
+s2=(input().lower()).split(' ')
+for i in s1:
+    for j in s2:
+        if i==j and s1.count(i)==1 and s2.count(j)==1:
+            a+=1
+print(a)
