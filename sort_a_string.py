60ab51d16f30f0ecdab5b9c8ba143889cdd073af
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-10-14 21:48:07 +0530

    s=input() a=[] for i in s:     if i.isalpha():         a.append(i) a.sort() j=0 for i in s:     if i.isalpha():         i=a[j]         j+=1     print(i,end='')

--- sort_a_string.py
+++ sort_a_string.py
@@ -4,9 +4,9 @@ for i in s:
     if i.isalpha():
         a.append(i)
 a.sort()
-c=0
+j=0
 for i in s:
     if i.isalpha():
-        i=a[c]
-        c+=1
+        i=a[j]
+        j+=1
     print(i,end='')
