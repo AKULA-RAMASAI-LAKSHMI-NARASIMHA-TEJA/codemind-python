aea3f96fb2595f7739f3d188e7a189bcd751de67
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-10-14 21:35:34 +0530

    s=input().split(' ') vow=['a','e','i','o','u','A','E','I','O','U'] a=0 for i in s:     s1=i[0:len(i)//2]     s2=i[len(i)//2:len(i)]     s2=s2[::-1]     for j in range(min(len(s1),len(s2))):         if (s1[j] in vow and s2[j] not in vow)or(s1[j] not in vow and s2[j] in vow):             a+=1 print(a)

--- count_vowel_consonent_pairs_in_words.py
+++ count_vowel_consonent_pairs_in_words.py
@@ -1,12 +1,11 @@
-s=input()
-s=s.split(' ')
-v=['a','e','i','o','u','A','E','I','O','U']
-c=0
+s=input().split(' ')
+vow=['a','e','i','o','u','A','E','I','O','U']
+a=0
 for i in s:
-    a=i[0:len(i)//2]
-    b=i[len(i)//2:len(i)]
-    b=b[::-1]
-    for j in range(min(len(a),len(b))):
-        if(a[j] in v and b[j] not in v) or (a[j] not in v and b[j] in v):
-            c+=1
-print(c)
+    s1=i[0:len(i)//2]
+    s2=i[len(i)//2:len(i)]
+    s2=s2[::-1]
+    for j in range(min(len(s1),len(s2))):
+        if (s1[j] in vow and s2[j] not in vow)or(s1[j] not in vow and s2[j] in vow):
+            a+=1
+print(a)
